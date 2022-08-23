4292192dcb6c3795c55f2541e4a0972ea6fd33eb
vlad <vlad.kharkovskiy10@gmail.com>
2022-08-23 19:53:52 +0300

    productinfo

--- js/product.js
+++ js/product.js
@@ -5,9 +5,11 @@ for (let i = 0; i < smallItems.length; i++) {
     smallItems[i].addEventListener('click', function () {
         mainItem.src = this.src;
         this.style.opacity = 0.5;
+        this.style.backgroundColor = '#000';
         for(let j = 0; j < smallItems.length; j++) {
             if (j != i) {
                 smallItems[j].style.opacity = 1;
+                smallItems[j].style.backgroundColor = '#E9E9E9';
             }
         }
     })
