da17b9439cf435d860765f8d7a3112cac9f51a6c
vlad <vlad.kharkovskiy10@gmail.com>
2022-08-24 12:07:00 +0300

    finish static

--- js/product.js
+++ js/product.js
@@ -26,17 +26,24 @@ let position = 0;
 function waitChange() {
     sliderLine.style.transition = 'none';
     sliderLine.style.left = -position + 'px';
+    document.getElementById('next').disabled = false;
+    document.getElementById('prev').disabled = false;
 }
 
 document.getElementById('next').addEventListener('click', function() {
     let flag = false;
+    
     sliderLine.style.transition = 'all ease 1s';
     position += 990;
     if (position > slider.offsetWidth * 4) {
         flag = true;
         sliderLine.style.left = -position + 'px';
         position = 0;
-        setTimeout(waitChange, 700);
+        document.getElementById('next').disabled = true;
+        document.getElementById('prev').disabled = true;
+        setTimeout(waitChange, 600);
+        
+        
     }
     if (!flag) {
         sliderLine.style.left = -position + 'px';
@@ -46,13 +53,16 @@ document.getElementById('next').addEventListener('click', function() {
 
 document.getElementById('prev').addEventListener('click', function() {
     let flag = false;
+    document.getElementById('next').disabled = false;
     sliderLine.style.transition = 'all ease 1s';
     position -= 990;
     if (position < 0) {
         flag = true;
         sliderLine.style.left = -position + 'px';
         position = slider.offsetWidth * 4;
-        setTimeout(waitChange, 700);
+        document.getElementById('next').disabled = true;
+        document.getElementById('prev').disabled = true;
+        setTimeout(waitChange, 600);
     }
     if (!flag) {
         sliderLine.style.left = -position + 'px';
