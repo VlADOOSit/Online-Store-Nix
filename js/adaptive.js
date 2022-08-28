7639b438795a2a04993d5307b4837b45a11342dd
vlad <vlad.kharkovskiy10@gmail.com>
2022-08-28 17:08:50 +0300

    adaptive

---
+++ js/adaptive.js
@@ -0,0 +1,15 @@
+document.getElementsByClassName('adaptive__basket')[0].onclick = function() {
+    window.location.href = './basket.html'
+} 
+
+if(document.getElementById('show__side')) {
+    document.getElementById('show__side').addEventListener('click', function() {
+        document.querySelector('.sidebar').classList.toggle('sidebar-toggle')
+    })
+}   
+
+
+
+document.getElementById('menu__btn').addEventListener('click', function() {
+    document.querySelector('.adaptive__header').classList.toggle('header-toggle')
+})
